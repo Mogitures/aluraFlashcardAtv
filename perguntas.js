27fa6dd3aa97448d368043eb50a853bfb3201c70
Davi <78176111+Mogitures@users.noreply.github.com>
2024-11-19 12:30:41 -0300

    AtvAlura

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual o significado de MVC?',
+    'Model, View e Controller.'
+)
+
+criaCartao(
+    'Ilustração',
+    'Quem é o criador da Hilda?',
+    'O artista Luke Pearson!'
+)
+
+criaCartao(
+    'Minecraft',
+    'O que acontece se pular e atacar um inimigo em seguida?',
+    'Causa dano crítico.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual o significado de "wonderful"?',
+    'Maravilhoso.'
+)
